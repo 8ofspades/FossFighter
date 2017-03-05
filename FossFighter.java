6d0e69f5d71c3a152c3f8727244d8710aef7846c
mani <m.angerhofer@ufl.edu>
2017-03-05 16:59:06 -0500

    simplified starting interface

--- FossFighter.java
+++ FossFighter.java
@@ -1,21 +1,17 @@
+//Main gain
 import java.util.*;
 
 public class FossFighter{
-
+	
+	//setup initial start conditions
     public static void main(String[] args){
 		System.out.println("FOSS FIGHTERS: The Ultimate FOSS Adventure");
 		Scanner input = new Scanner(System.in);
 		Processor processor = new Processor();
 		String command = ""; 
-//		Location location = new Location();
 		Location.setlocation("church of emacs");
-//		Conversation converstation  = new Conversation();
-//		Software mysoftware;
-//		Software enemysoftware;
-//		mysoftware = new Software();
-//		enemysoftware = new Software();
 		System.out.println(Global.info);
-		//	Repository repository = new Repository();
+		//process commands until quit is typed
 		while (!command.equals("quit")){
 		    command  = input.nextLine();
 		    processor.processCommand(command);
