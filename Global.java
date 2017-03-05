e6f430e2b2c1711a32012b3cad150c270d3f56d6
mani <m.angerhofer@ufl.edu>
2017-03-05 16:59:35 -0500

    added some variable explanations

--- Global.java
+++ Global.java
@@ -7,6 +7,6 @@ public class Global{
     public static String mysoftware;
     public static String enemysoftware;
     public static Boolean conversationEnd;
-    public static int gamestate = 0;
+    public static int gamestate = 0; //Combat state, 0 = ur dead, 1 = enemy ded, 2 = ongoing battle
     public static int health;
 }
